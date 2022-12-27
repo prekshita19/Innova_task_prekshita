98307a13bcd8dcacc752abb03665512d0e944b12
prekshita <prekshitavpatil@gmail.com>
2022-12-27 17:04:28 +0530

    new

--- image_face_detect.py
+++ image_face_detect.py
@@ -1,90 +1,29 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 8,
-   "id": "07781b1c",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "-1"
-      ]
-     },
-     "execution_count": 8,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "#importing open cv library\n",
-    "\n",
-    "import cv2\n",
-    "import numpy as np\n",
-    "\n",
-    "#dataset load \n",
-    "trainedData=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')\n",
-    "\n",
-    "#choose a image\n",
-    "\n",
-    "img=cv2.imread('one.jpg')\n",
-    "\n",
-    "#Conversion to black and white (grayscale)\n",
-    "\n",
-    "#grayimg=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)\n",
-    "#print(grayimg)\n",
-    "\n",
-    "#detect faces\n",
-    "\n",
-    "faceCoordinates = trainedData.detectMultiScale(img)\n",
-    "#print(faceCoordinates)\n",
-    "  \n",
-    "for i in range(0,9): \n",
-    "\n",
-    "    x,y,w,h=faceCoordinates[i]\n",
-    "    cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)\n",
-    "\n",
-    "cv2.imshow('person', img) \n",
-    "cv2.waitKey()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "9f31e200",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "e4628bb0",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#importing open cv library
+
+import cv2
+import numpy as np
+
+#dataset load 
+trainedData=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#choose a image
+
+img=cv2.imread('one.jpg')
+
+#Conversion to black and white (grayscale)
+
+#grayimg=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#print(grayimg)
+
+#detect faces
+
+faceCoordinates = trainedData.detectMultiScale(img)
+#print(faceCoordinates)
+  
+for i in range(0,9): 
+
+    x,y,w,h=faceCoordinates[i]
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
+
+cv2.imshow('person', img) 
+cv2.waitKey()
--- video_face_detect.py
+++ video_face_detect.py
@@ -1,90 +1,32 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "d36db863",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#importing open cv library\n",
-    "\n",
-    "import cv2\n",
-    "from IPython.display import Video\n",
-    "\n",
-    "from random import randrange as r \n",
-    "#dataset load\n",
-    "\n",
-    "trainedData=cv2.CascadeClassifier(\"haarcascade_frontalface_default.xml\")\n",
-    "\n",
-    "#start the webcam\n",
-    "cam = cv2.VideoCapture(\"video1.mp4\")\n",
-    "\n",
-    "while True:\n",
-    "\n",
-    "    success,frame=cam.read()\n",
-    "\n",
-    "#Conversion to black and white(grayscale) \n",
-    "#graying=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)\n",
-    "\n",
-    "#detect faces\n",
-    "faceCoordinates=trainedData.detectMultiScale(graying)\n",
-    "\n",
-    "for x,y,w,h in faceCoordinates: \n",
-    "    cv2.rectangle(frame, (x,y), (x+w, y+h), (r(0,256),r(0,256),r(0,256)),2)\n",
-    "\n",
-    "cv2.imshow('frame', frame)\n",
-    "key=cv2.waitKey(1) \n",
-    "if (key==81 or key==113):\n",
-    "    break\n",
-    "\n",
-    "cam.release()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "84e41135",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "990df7ac",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "651e74f7",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#importing open cv library
+
+import cv2
+from IPython.display import Video
+
+from random import randrange as r 
+#dataset load
+
+trainedData=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+#start the webcam
+cam = cv2.VideoCapture("video1.mp4")
+
+while True:
+
+    success,frame=cam.read()
+
+#Conversion to black and white(grayscale) 
+#graying=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+#detect faces
+faceCoordinates=trainedData.detectMultiScale(graying)
+
+for x,y,w,h in faceCoordinates: 
+    cv2.rectangle(frame, (x,y), (x+w, y+h), (r(0,256),r(0,256),r(0,256)),2)
+
+cv2.imshow('frame', frame)
+key=cv2.waitKey(1) 
+if (key==81 or key==113):
+    break
+
+cam.release()
